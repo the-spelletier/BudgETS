5b580b1efbee0db57f2ad25e3197686dea1485f5
Michael Dowse <michael.dowse.1@ens.etsmtl.ca>
2020-09-24 00:57:08 -0400

    Implement budget service with CRUD

---
+++ services/budget.js
@@ -0,0 +1,38 @@
+const Budget = require('../models').Budget;
+
+// Retourne un budget (tous les paramètres) selon l'identificateur envoyé en paramètre
+const getBudget = budget => {
+    return Budget.findOne({
+        where: budget
+    });
+}
+
+// Ajout d'un budget
+const addBudget = budget => {
+    return Budget.create(budget);
+}
+
+// Mise à jour d'un budget selon l'identificateur envoyé en paramètre
+const updateBudget = budget => {
+    return Budget.update(budget, { 
+        where: { 
+            id: budget.id 
+        } 
+    });
+}
+
+// Suppression d'un budget selon l'identificateur envoyé en paramètre
+const deleteBudget = budget => {
+    return Budget.destroy({ 
+        where: { 
+            id: budget.id 
+        } 
+    });
+}
+
+module.exports = {
+    getBudget,
+    addBudget,
+    updateBudget,
+    deleteBudget
+};
