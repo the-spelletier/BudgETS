f6a6c9c7ec6476b7d56f8ee50a4fdbf5e8d7e3df
Michael Dowse <michael.dowse.1@ens.etsmtl.ca>
2020-09-24 15:07:21 -0400

    Implement createUser inside user controller

--- controllers/user.js
+++ controllers/user.js
@@ -9,7 +9,20 @@ function getAll(req, res) {
 }
 
 function create(req, res) {
-
+    if (req.body.user && req.body.user.username && req.body.user.password) {
+        userService.addUser(req.body.user).then((result) => {
+            console.log(result);
+            res.status(200).send(result);
+        }).catch(err => {
+            res.status(401).send({
+                message: err.message
+            });
+        });
+    } else {
+        return res.status(403).send({ 
+            message: 'Invalid parameters' 
+        });
+    }
 }
 
 function update(req, res) {
