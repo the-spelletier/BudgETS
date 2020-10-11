ea3d8515436a1de3c02c2b9be93dc75e5f43afec
Michael Dowse <michael.dowse.1@ens.etsmtl.ca>
2020-10-11 00:38:09 -0400

    Implement controller methods for entries

--- controllers/entry.js
+++ controllers/entry.js
@@ -1,13 +1,22 @@
+const { entryDTO } = require('../dto');
+const entryService = require('../services/entry');
 
 function get(req, res) {
-    res.status(500);
-    res.send({
-        message: "Not implemented"
+    entryService.getEntry(entryDTO(req.params)).then(entry => {
+        sendEntry(entry, res);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send({ message: 'An unexpected error occurred' });
     });
 }
 
 function getAll(req, res) {
-
+    entryService.getEntries(req.params.budgetId).then(entries => {
+        sendEntry(entries, res);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send({ message: 'An unexpected error occurred' });
+    });
 }
 
 function getRevenues(req, res) {
@@ -19,15 +28,65 @@ function getExpenses(req, res) {
 }
 
 function create(req, res) {
-
+    let entry = entryDTO(req.body);
+    if (entry.lineId, entry.amount, entry.date, entry.member, entry.description, entry.type) { 
+        entryService.addEntry(entry).then(e => {
+            res.status(201);
+            sendEntry(e, res);
+        }).catch(err => {
+            res.status(403).send({ message: 'Validation error' });
+        });
+    } else {
+        res.status(400).send({ message: 'Invalid parameters' });
+    }
 }
 
 function update(req, res) {
-
+    let entry = entryDTO(req.body);
+    if (req.params.id) {
+        entry.id = req.params.id;
+        entryService.updateEntry(entry).then(e => {
+            sendEntry(e, res);
+        }).catch(err => {
+            res.status(403).send({ message: 'Validation error' });
+        });
+    } else {
+        res.status(400).send({ message: 'Invalid parameters' });
+    }
 }
 
 function deleteOne(req, res) {
+    let entry = entryDTO(req.params);
+    if (entry.id) {
+        entryService.deleteEntry(entry).then(result => {
+            if (result) {
+                res.sendStatus(204);
+            } else {
+                res.status(404).send({ message: "Entry Not Found" });
+            }
+        }).catch(err => {
+            res.status(403).send({ message: 'Validation error' });
+        });
+    } else {
+        res.status(400).send({ message: 'Invalid parameters' });
+    }
+}
 
+function sendEntry(entry, res) {
+    if (entry) {
+        let entryRes;
+        if (Array.isArray(entry)) {
+            entry.forEach((b, i, arr) => {
+                arr[i] = entryDTO(b);
+            });
+            entryRes = entry;
+        } else {
+            entryRes = entryDTO(entry);
+        }
+        res.send(entryRes);
+    } else {
+        res.status(404).send({ message: "Entry Not Found" });
+    }
 }
 
 module.exports = {
