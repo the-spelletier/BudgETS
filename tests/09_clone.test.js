3009bd1cab52003e955d47fad3e81b99f9775e51
pcuerrier <philippe.cuerrier@gmail.com>
2020-10-23 10:22:00 -0400

    Fix bugs avec les tests 9.0
    Pour cloner un budget, on doit envoyer un nouveau nom et des nouvelles date de début et de fin.
    
    Ajotu de tess pour tester les paramètres à envoyer.

--- tests/09_clone.test.js
+++ tests/09_clone.test.js
@@ -11,8 +11,13 @@ const stubAuth = sinon.stub(auth, 'verifyAuth');
 const app = require('../app.js');
 
 describe('9.0 - Clonage de budget', () => {
+    beforeEach(() => {
+        // Original authentification service
+        auth.verifyAuth.callsFake(originalAuth);
+    });
+
     describe('9.1 - Route pour cloner', () => {
-        test('091001 - Route vers cloner un budget', (done) => {
+        test('091001 - Cloner un budget', (done) => {
             // Stub the verifyAuth
             auth.verifyAuth.callsFake((req, res, next) => {
                 userService.getUser({
@@ -22,10 +27,32 @@ describe('9.0 - Clonage de budget', () => {
                     next();
                 })
             });
+            
+            const name = 'Budget Clone 1'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
 
             request(app)
                 .post('/api/budget/clone/5')
-                .expect(200, done);
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
+                .expect(200)
+                .then((response) => {
+                    expect(response.body.id).not.toBeUndefined();
+                    expect(response.body).toEqual(
+                        expect.objectContaining({
+                            name: name,
+                            startDate: start,
+                            endDate: end,
+                            isActive: false,
+                            userId: '2'
+                        })
+                    );
+                    done();
+                });
         });
 
         test('091002 - Route vers cloner un budget sans id', (done) => {
@@ -39,59 +66,39 @@ describe('9.0 - Clonage de budget', () => {
                 })
             });
 
+            const name = 'Budget Clone 2'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
             request(app)
-                .post('/api/budget/clone')
+                .post('/api/budget/clone/')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
                 .expect(404, done);
         });
     });
 
     describe('9.2 - Cloner un budget', () => {
-        test('092001 - Cloner un budget', (done) => {
-            // Stub the verifyAuth
-            auth.verifyAuth.callsFake((req, res, next) => {
-                userService.getUser({
-                    username:'budgets_test002'
-                }).then(user => {
-                    req.user = User.build(user, {raw: true});
-                    next();
-                })
-            });
-
-            const start = new Date(2020, 0, 1).toJSON();
-            const end = new Date(2020, 11, 31).toJSON();
-
-            request(app)
-                .post('/api/budget/clone/5')
-                .expect(200)
-                .then((response) => {
-                    expect(response.body.id).not.toBeUndefined();
-                    expect(response.body).toEqual(
-                        expect.objectContaining({
-                            name: 'budgetTest00201',
-                            startDate: start,
-                            endDate: end,
-                            isActive: true,
-                            userId: '2'
-                        })
-                    );
-                    done();
-                });
-        });
-
         test("092001 - Cloner un budget sans authentification", (done) => {
-            // Original authentification service
-            auth.verifyAuth.callsFake(originalAuth);
-
-            const start = new Date(2020, 0, 1).toJSON();
-            const end = new Date(2020, 11, 31).toJSON();
+            const name = 'Budget Clone 3'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
 
             request(app)
                 .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
                 .expect(401, done);
             
         });
 
-        test('092003 - Cloner un budget qui ne m\'appartient pas', (done) => {
+        test('092002 - Cloner un budget qui ne m\'appartient pas', (done) => {
             // Stub the verifyAuth
             auth.verifyAuth.callsFake((req, res, next) => {
                 userService.getUser({
@@ -102,12 +109,21 @@ describe('9.0 - Clonage de budget', () => {
                 })
             });
 
+            const name = 'Budget Clone 4'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
             request(app)
-                .post('/api/budget/clone/5')
+                .post('/api/budget/clone/9')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
                 .expect(404, done);
         });
 
-        test('092004 - Cloner un budget qui n\'existe pas', (done) => {
+        test('092003 - Cloner un budget qui n\'existe pas', (done) => {
             // Stub the verifyAuth
             auth.verifyAuth.callsFake((req, res, next) => {
                 userService.getUser({
@@ -118,9 +134,212 @@ describe('9.0 - Clonage de budget', () => {
                 })
             });
 
+            const name = 'Budget Clone 5'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
             request(app)
                 .post('/api/budget/clone/-1')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
                 .expect(404, done);
+        });
+
+        test('092004 - Cloner un budget sans nom', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    startDate: start,
+                    endDate: end
+                })
+                .expect(400, done);
+        });
+
+        test('092005 - Cloner un budget avec nom vide', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = ''
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
+                .expect(400, done);
+        });
+
+        test('092006 - Cloner un budget sans date de début', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = 'Budget Clone 6'
+            const end = '2021-09-30 23:59:59'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    endDate: end
+                })
+                .expect(400, done);
+        });
+
+        test('092007 - Cloner un budget avec date de début vide', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = 'Budget Clone 7'
+            const start = ''
+            const end = '2021-09-30 23:59:59'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
+                .expect(400, done);
+        });
+
+        test('092008 - Cloner un budget sans date de fin', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = 'Budget Clone 8'
+            const start = '2021-09-25 00:00:00'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: start
+                })
+                .expect(400, done);
+        });
+
+        test('092009 - Cloner un budget avec date de fin vide', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = 'Budget Clone 9'
+            const start = '2021-09-25 00:00:00'
+            const end = ''
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
+                .expect(400, done);
+        });
+
+        test('092010 - Cloner un budget avec date de fin < date de début', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = 'Budget Clone 10'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: end,
+                    endDate: start
+                })
+                .expect(400, done);
+        });
+
+        test('092011 - Cloner un budget avec nom non unique', (done) => {
+            // Stub the verifyAuth
+            auth.verifyAuth.callsFake((req, res, next) => {
+                userService.getUser({
+                    username:'budgets_test002'
+                }).then(user => {
+                    req.user = User.build(user, {raw: true});
+                    next();
+                })
+            });
+
+            const name = 'budgetTest00201'
+            const start = '2021-09-25 00:00:00'
+            const end = '2021-09-30 23:59:59'
+
+            request(app)
+                .post('/api/budget/clone/5')
+                .send({
+                    name: name,
+                    startDate: start,
+                    endDate: end
+                })
+                .expect(403, done);
         });
 
     });
