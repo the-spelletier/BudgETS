b2c195e9c0d4bc1a87afb54aae5a8d3f19af43f1
Simon <pelletier.simon.18@gmail.com>
2020-11-18 16:34:00 -0500

    Added new page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import { Layout } from 'antd';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Layout, Button } from 'antd';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import FooterToolbar from 'ant-design-pro/lib/FooterToolbar';
 import AppRouter from "./AppRouter";
 import Sidebar from "./Sidebar";
 import UserContextProvider from "./contexts/user/UserContextProvider";
 
 import 'antd/dist/antd.css';
 import './App.scss';
+import 'ant-design-pro/dist/ant-design-pro.css';
 
-const { Content, Sider } = Layout;
+const { Content, Sider, Footer } = Layout;
 
 const App = () => {
     return (
         <Layout className="app">                        
         <UserContextProvider>
             <Router>
-                <Sider className="sidebar">
-                <div className="logo">BudgETS</div>
-                    <Sidebar />
-                </Sider>
                 <Layout className="content">
+                    <Sider className="sidebar">
+                    <div className="logo">BudgETS</div>
+                        <Sidebar />
+                    </Sider>
                     <Content style={{ margin: '24px 16px 0' }}>
                         <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
                                 <AppRouter/>
                         </div>
                     </Content>
+                    <FooterToolbar className="footbar">
+                        <Link to="/help">Aide</Link>
+                        <Link to="/about">À propos</Link>
+                    </FooterToolbar>
                 </Layout>
             </Router>                    
         </UserContextProvider>
