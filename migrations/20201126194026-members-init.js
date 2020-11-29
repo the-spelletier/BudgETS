0e648d89ad51325496f74ea9ba32ab9a098b0dea
Michael Dowse <michael.dowse.1@ens.etsmtl.ca>
2020-11-28 23:14:31 -0500

    Add missing fields to member table

--- migrations/20201126194026-members-init.js
+++ migrations/20201126194026-members-init.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.query("CREATE TABLE IF NOT EXISTS `Members` (`id` CHAR(36) BINARY , `name` VARCHAR(255) NOT NULL, `code` VARCHAR(255) NOT NULL UNIQUE, `email` VARCHAR(255) NOT NULL, `userId` CHAR(36) BINARY NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`userId`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE) ENGINE=InnoDB;");
+    return queryInterface.sequelize.query("CREATE TABLE IF NOT EXISTS `Members` (`id` CHAR(36) BINARY , `name` VARCHAR(255) NOT NULL, `code` VARCHAR(255) NOT NULL UNIQUE, `email` VARCHAR(255), `sms` VARCHAR(255), `notify` TINYINT(1) DEFAULT false, `active` TINYINT(1) DEFAULT true, `userId` CHAR(36) BINARY NOT NULL, `deleted` TINYINT(1) DEFAULT false, PRIMARY KEY (`id`), FOREIGN KEY (`userId`) REFERENCES `Users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE) ENGINE=InnoDB;");
   },
 
   down: async (queryInterface, Sequelize) => {
